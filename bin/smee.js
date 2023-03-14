0c8343115589d9e0bdbf8e6d3aea7c02b3a36936
Jakob J. Kolb <jakob.j.kolb@gmail.com>
2023-03-14 14:37:50 +0100

    Refactor app.

--- bin/smee.js
+++ bin/smee.js
@@ -12,24 +12,18 @@ program
   .option('-t, --target <target>', 'Full URL (including protocol and path) of the target service the events will forwarded to. Default: http://127.0.0.1:PORT/PATH')
   .option('-p, --port <n>', 'Local HTTP server port', process.env.PORT || 3000)
   .option('-P, --path <path>', 'URL path to post proxied requests to`', '/')
-  .parse(process.argv)
 
-let target
-if (program.target) {
-  target = program.target
-} else {
-  target = `http://127.0.0.1:${program.port}${program.path}`
-}
+program.parse()
 
-async function setup () {
-  let source = program.url
+async function setup (program) {
+  const options = program.opts()
 
-  if (!source) {
-    source = await Client.createChannel()
-  }
+  const source = options.url || await Client.createChannel()
+  const target = options.target || `http://127.0.0.1:${options.port}${options.path}`
 
   const client = new Client({ source, target })
   client.start()
+  return client
 }
 
-setup()
+setup(program)
