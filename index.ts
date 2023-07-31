3645e685849cb2e2a185671cc82270687fd0a92c
Jakob J. Kolb <jakob.j.kolb@gmail.com>
2023-07-31 17:14:12 +0200

    Add health endpoint.

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ import validator from 'validator'
 import EventSource from 'eventsource'
 import superagent from 'superagent'
 import url from 'url'
+import express from 'express'
 import querystring from 'querystring'
 import 'global-agent/bootstrap'
+import http from 'http'
 
 type Severity = 'info' | 'error'
 
@@ -20,6 +22,7 @@ class Client {
   host: string;
   logger: Pick<Console, Severity>;
   events!: EventSource;
+  server!: http.Server;
 
   constructor ({ source, target, host, logger = console }: Options) {
     this.source = source
@@ -34,8 +37,27 @@ class Client {
 
   static async createChannel () {
     return superagent.head('https://smee.io/new').redirects(0).catch((err) => {
+      console.log(err)
       return err.response.headers.location
     })
+  }
+
+  createHealthListener () {
+
+    const app = express()
+
+    app.get('/health', (req, res) => {
+      this.logger.info('health check')
+      this.logger.info(this.events.readyState, this.events.OPEN)
+      if (this.events.readyState == this.events.CLOSED || this.events.readyState == this.events.CONNECTING) {
+        res.status(503).send('Event source is not connected')
+        return res
+      }
+      res.status(200).send('OK')
+      return res
+    })
+
+    return app
   }
 
   onmessage (msg: any) {
@@ -69,18 +91,18 @@ class Client {
   }
 
   onopen () {
-    this.logger.info('Connected', this.events.url)
+    // this.logger.info('Connected', this.events.url)
   }
 
   onerror (err: any) {
-    this.logger.error(err)
+    // this.logger.error(err)
   }
 
   start () {
     const events = new EventSource(this.source);
 
     // Reconnect immediately
-    (events as any).reconnectInterval = 0 // This isn't a valid property of EventSource
+    // (events as any).reconnectInterval = 0 // This isn't a valid property of EventSource
 
     events.addEventListener('message', this.onmessage.bind(this))
     events.addEventListener('open', this.onopen.bind(this))
@@ -90,8 +112,20 @@ class Client {
     this.logger.info(`Setting host to ${this.host}`)
     this.events = events
 
+    const app = this.createHealthListener()
+
+    this.server = app.listen(3000, () => {
+      this.logger.info('Health listener started on port 3000')
+    })
+
     return events
   }
+
+  async stop () {
+    this.events.close()
+    await this.server.close()
+}
+
 }
 
 export = Client
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,12 @@
 import Client = require('..')
 import nock = require('nock')
+import superagent = require('superagent')
+import EventSource from 'eventsource'
+
+beforeAll(() => {
+  nock.disableNetConnect()
+  nock.enableNetConnect('localhost')
+})
 
 describe('client', () => {
   describe('createChannel', () => {
@@ -11,6 +18,63 @@ describe('client', () => {
       const channel = await Client.createChannel()
       expect(channel).toEqual('https://smee.io/abc123')
       expect(req.isDone()).toBe(true)
+    })
+  })
+  describe('start', () => {
+    test('returns an event source', async () => {
+
+      const client = new Client({
+        source: 'https://smee.io/abc123',
+        target: 'https://example.com',
+        host: 'smee.io'
+      })
+
+      let events = client.start()
+      expect(events).toBeInstanceOf(EventSource)
+
+      await client.stop()
+    })
+
+    test('starts a listener on localhost:3000/health', async () => {
+
+      nock('https://smee.io').get('/abc123').reply(200, '')
+
+      const client = new Client({
+        source: 'https://smee.io/abc123',
+        target: 'https://example.com',
+        host: 'smee.io'
+      })
+
+      client.start()
+
+      // expect response that is not a 404
+      await superagent.get('localhost:3000/health').catch((err) => {
+        expect(err.status).not.toEqual(404)
+      })
+
+      await client.stop()
+
+    })
+
+    test('returns a 503 if the event source is not connected', async () => {
+
+      nock('https://smee.io').get('/abc123').reply(200, '')
+
+      const client = new Client({
+        source: 'https://smee.io/abc123',
+        target: 'https://example.com',
+        host: 'smee.io'
+      })
+
+      const events = client.start()
+      events.close()
+
+      // expect a 503 response
+      await superagent.get('localhost:3000/health').catch((err) => {
+        expect(err.message).toContain('Service Unavailable')
+      })
+
+      await client.stop()
     })
   })
 })
